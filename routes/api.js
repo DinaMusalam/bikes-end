d012b6c2e094c74b4cd691014c06ca840766f4ef
goodCat56 <pythontech56@gmail.com>
2016-10-24 20:40:32 +0200

    second init commit

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const path = require('path');
+const connectionString = 'postgres://pzzkwpsqggsmig:CGOQ6vuw7ecylxEiuWJ2eIElMN@ec2-54-243-203-85.compute-1.amazonaws.com:5432/d67oe9r9t3ce0';
+
+pg.defaults.ssl = true;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with json');
+router.get('/users', function(req, res, next) {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+    done();
+    console.log(err);
+    return res.status(500).json({success: false, data: err});
+  }
+    // SQL Query > select users
+    const query =client.query('SELECT user_id, created_atherou FROM users LIMIT 20');
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+  });
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+    done();
+  return res.json(results);
+  });
+  });
 });
 
 module.exports = router;
